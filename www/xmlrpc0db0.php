d51671adab7728719defef9b92ddea324ed7f86a
Suraj Shenoy <ss05203@surrey.ac.uk>
2025-05-26 14:49:34 +0100

    added copied website from original

---
+++ www/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://groups.surreyunion.org/EARS</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://groups.surreyunion.org/EARS/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://groups.surreyunion.org/EARS/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://groups.surreyunion.org/EARS/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://groups.surreyunion.org/EARS/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://groups.surreyunion.org/EARS/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
